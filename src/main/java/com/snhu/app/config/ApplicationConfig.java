4fd0402dcc3c5138fd00edd31b31fe250c801f32
Reta232 <3344486+Reta232@users.noreply.github.com>
2018-08-12 00:41:14 -0500

    fixed logger maybe

--- src/main/java/com/snhu/app/config/ApplicationConfig.java
+++ src/main/java/com/snhu/app/config/ApplicationConfig.java
@@ -4,9 +4,13 @@ import java.net.UnknownHostException;
 
 import com.mongodb.MongoClient;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InjectionPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 /**
  * ApplicationConfig
@@ -18,5 +22,11 @@ public class ApplicationConfig {
 	@Bean
 	public MongoClient mongoClient() throws UnknownHostException {
 		return new MongoClient( "localhost" );
+	}
+
+	@Bean
+	@Scope( "prototype" )
+	public Logger logger( InjectionPoint injectionPoint ) {
+		return LoggerFactory.getLogger( injectionPoint.getMember().getDeclaringClass() );
 	}
 }
