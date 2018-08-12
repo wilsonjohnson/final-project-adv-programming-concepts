2edd5b33cb7c04bf3e883968b1147e27951112ba
Reta232 <3344486+Reta232@users.noreply.github.com>
2018-08-12 15:19:50 -0500

    updated adapter to accept weird date formats

--- src/main/java/com/snhu/app/adapter/InspectionAdapter.java
+++ src/main/java/com/snhu/app/adapter/InspectionAdapter.java
@@ -1,5 +1,9 @@
 package com.snhu.app.adapter;
 
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -20,10 +24,24 @@ public class InspectionAdapter implements Adapter<Inspection> {
 
 	private Logger log = LoggerFactory.getLogger( this.getClass() );
 
+	private static final DateTimeFormatter FORMAT =  DateTimeFormatter.ofPattern("MMM dd yyyy");
+
 	@Override
 	public Optional< Inspection > toJava( DBObject object ) {
 		try {
 			ObjectMapper mapper = new ObjectMapper();
+			Object date = object.get("date");
+			if( date instanceof String ){
+				String temp = (String) date;
+				if( temp == null || temp.isEmpty() ){
+					object.put( "date", null );
+				} else {
+					object.put( "date", LocalDateTime.parse( temp, FORMAT ) );
+				}
+			} else if( date instanceof Date ){
+				Date temp = (Date) date;
+				object.put( "date", Timestamp.from( temp.toInstant() ).toLocalDateTime() );
+			}
 			return Optional.ofNullable( mapper.readValue( object.toString() , Inspection.class ) );
 		} catch ( Exception e ){
 			log.error( "", e );
